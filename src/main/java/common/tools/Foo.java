b53ef436360dea12223ec1d4a117c6fc21c3d885
renqun.yuan <renqun.yuan@qunar.com>
2017-04-14 15:54:09 +0800

    生成setter方法

---
+++ src/main/java/common/tools/Foo.java
@@ -0,0 +1,25 @@
+package common.tools;
+
+/**
+ * Created by yuanrq on 2017/04/14.
+ */
+class Foo {
+    private int id;
+    private String  name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/common/tools/GenerateSetters.java
@@ -0,0 +1,32 @@
+package common.tools;
+
+import com.google.common.base.CaseFormat;
+import com.google.common.base.Joiner;
+
+import java.lang.reflect.Field;
+
+/**
+ * 生成set方法，便于进行逻辑复制
+ * <p>
+ * Created by yuanrq on 2017/04/14.
+ */
+public class GenerateSetters {
+    private static final Joiner joiner_space = Joiner.on(" ");
+    private static final Joiner joiner_no_space = Joiner.on("");
+
+    public static void generateSetters(Class clazz) {
+        String simpleName = clazz.getSimpleName();
+        String declareName = CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_CAMEL, simpleName);
+        System.out.println(joiner_space.join(simpleName, declareName, "=new", simpleName, "();"));
+        Field[] declaredFields = clazz.getDeclaredFields();
+        for (Field declaredField : declaredFields) {
+            System.out.println(joiner_no_space.join(declareName, ".", "set", CaseFormat.LOWER_CAMEL.to(CaseFormat.UPPER_CAMEL, declaredField.getName()), "();"));
+        }
+    }
+
+    public static void main(String[] args) {
+        generateSetters(Foo.class);
+    }
+
+
+}
